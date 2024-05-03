2a95e8125b31a94fd65a152cc7b1b41226096a13
MPS Vihanga <>
2024-05-03 13:41:44 +0530

    05-Primary

---
+++ 02-SpringBoot-Core/05-primary/src/main/java/com/example/springcoredemo/common/BaseballCoach.java
@@ -0,0 +1,11 @@
+package com.example.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BaseballCoach implements Coach{
+    @Override
+    public String getDailyWorkout() {
+        return "Practice for Baseball in 30min";
+    }
+}
