f603428b2eadd41f601909efae566e8417d168ff
MPS Vihanga <>
2024-05-03 13:42:25 +0530

    06-Lazy-Initialization

---
+++ 02-SpringBoot-Core/06-lazy-initialization/src/main/java/com/example/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,18 @@
+package com.example.springcoredemo.common;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Component
+@Lazy
+public class CricketCoach implements Coach{
+
+    public CricketCoach() {
+        System.out.println("in Constructor:" + getClass().getSimpleName());
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice for Cricket in 15min";
+    }
+}
